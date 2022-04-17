3dc743684bd699cc1d2970a772b82d78fae48b63
moamen-montaser <moamenmontaser12@gmail.com>
2022-04-17 23:47:27 +0200

    New

---
+++ Test.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+	struct process 
+	{
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int priority;
+    int start_time;
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
+    int response_time;
+	};
+	struct process p[100];
+	
+	// TO select type of scheduler
+	cout << "press 1 for FCFS \n2 for SJF preemptive\n3 for SJF non-preemptive \n4 for Priority preemptive \n5 for Priority non_preemptive \n6 for RR:  \n";
+	int x;
+	cin >> x;
+	
+	
+	//First algorithms FCFS 
+	
+	if (x == 1)
+	{
+		
+		
+		
+	}
+	
+
+}
