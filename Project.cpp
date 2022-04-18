6cbefe2194162689a4b99a5d655f9180a5fd230c
moamen-montaser <moamenmontaser12@gmail.com>
2022-04-18 20:05:12 +0200

    FCFS, and SJF

--- Project.cpp
+++ Project.cpp
@@ -101,7 +101,133 @@ int main ()
 	// SJF Preemptive
 	else if (x == 2)
 	{
-		
+		struct process p2[100];
+		float avrw2;
+		float avrt2;
+		float avrr2;
+		float cpu_utilization;
+		int total_turnaround_time = 0;
+		int total_waiting_time = 0;
+		int total_response_time = 0;
+		int total_idle_time = 0;
+		float throughput;
+		int burst_remaining[100];
+		int is_completed[100];
+
+		memset(is_completed, 0, sizeof(is_completed));
+
+		cout << setprecision(2) << fixed;
+		int num2;
+		cout << "Enter the number of processes: ";
+		cin >> num2;
+
+		for (int i = 0; i < num2; i++) {
+			cout << "Enter arrival and burst time of process " << i + 1 << ": ";
+			cin >> p2[i].arrival_time;
+			cin >> p2[i].burst_time;
+			p2[i].pid = i + 1;
+			burst_remaining[i] = p2[i].burst_time;
+		}
+
+		int current_time = 0;
+		int completed = 0;
+		int prev = 0;
+
+		while (completed != num2) {
+			int idx = -1;
+			int mn = 10000000;
+			for (int i = 0; i < num2; i++) {
+				if (p2[i].arrival_time <= current_time && is_completed[i] == 0) {
+					if (burst_remaining[i] < mn) {
+						mn = burst_remaining[i];
+						idx = i;
+					}
+					if (burst_remaining[i] == mn) {
+						if (p2[i].arrival_time < p2[idx].arrival_time) {
+							mn = burst_remaining[i];
+							idx = i;
+						}
+					}
+				}
+			}
+
+			if (idx != -1) {
+				if (burst_remaining[idx] == p2[idx].burst_time) {
+					p2[idx].start_time = current_time;
+					total_idle_time += p2[idx].start_time - prev;
+				}
+				burst_remaining[idx] -= 1;
+				current_time++;
+				prev = current_time;
+
+				if (burst_remaining[idx] == 0) {
+					p2[idx].completion_time = current_time;
+					p2[idx].turnaround_time = p2[idx].completion_time - p2[idx].arrival_time;
+					p2[idx].waiting_time = p2[idx].turnaround_time - p2[idx].burst_time;
+					p2[idx].response_time = p2[idx].start_time - p2[idx].arrival_time;
+
+					total_turnaround_time += p2[idx].turnaround_time;
+					total_waiting_time += p2[idx].waiting_time;
+					total_response_time += p2[idx].response_time;
+
+					is_completed[idx] = 1;
+					completed++;
+				}
+			}
+			else {
+				current_time++;
+			}
+		}
+
+		int min_arrival_time = 10000000;
+		int max_completion_time = -1;
+		for (int i = 0; i < num2; i++) {
+			min_arrival_time = min(min_arrival_time, p2[i].arrival_time);
+			max_completion_time = max(max_completion_time, p2[i].completion_time);
+		}
+
+		avrt2 = (float)total_turnaround_time / x;
+		avrw2 = (float)total_waiting_time / x;
+		avrr2 = (float)total_response_time / x;
+		cpu_utilization = ((max_completion_time - total_idle_time) / (float)max_completion_time) * 100;
+		throughput = float(x) / (max_completion_time - min_arrival_time);
+
+		cout << endl << endl;
+
+		cout << "pid" << "               ";
+		for (int i = 0; i < num2; i++)
+		{
+			cout << p2[i].pid << "       ";
+		}
+		cout << endl << endl;
+		cout << "arrival_time" << "      ";
+		for (int i = 0; i < num2; i++)
+		{
+			cout << p2[i].arrival_time << "       ";
+		}
+		cout << endl << endl;
+		cout << "burst_time" << "        ";
+		for (int i = 0; i < num2; i++)
+		{
+			cout << p2[i].burst_time << "       ";
+		}
+		cout << endl << endl;
+		cout << "turnaround_time" << "   ";
+		for (int i = 0; i < num2; i++)
+		{
+			cout << p2[i].turnaround_time << "       ";
+		}
+		cout << endl << endl;
+		cout << "waiting_time" << "      ";
+		for (int i = 0; i < num2; i++)
+		{
+			cout << p2[i].waiting_time << "       ";
+		}
+		cout << endl << endl;
+		avrw2 = avrw2 / num2;
+		avrt2 = avrt2 / num2;
+		cout << "Average waiting time = " << avrw2 << endl;
+		cout << "Average turnarround time = " << avrt2 << endl;		
 		
 		
 	}
